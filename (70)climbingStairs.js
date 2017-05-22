32b10c2f0c2f88b81ca8a80dd0ad1e7c9e33c85b
Chang Meng <mercyme@users.noreply.github.com>
2017-05-22 14:21:36 -0400

    Update (70)climbingStairs.js

--- (70)climbingStairs.js
+++ (70)climbingStairs.js
@@ -4,14 +4,11 @@
  */
 //better 
  var climbStairs = function(n) {
-   let result = [1];
-   if(n >= 2){
-     result[1] = 2;
-   }
-   for(let i = 2; i < n; i++){
+   let result = [1,1];
+   for(let i = 2; i <= n; i++){
      result[i] = result[i-1] + result[i-2];
    }
-   return result[i-1];
+   return result[n];
  }
 // recursion
   var climbStairsRecursion = function(n) {
