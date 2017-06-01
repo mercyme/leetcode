342738f3c32ccbe6e8a8cdf2c1249ae08e5f4359
Chang Meng <mercyme@users.noreply.github.com>
2017-06-01 10:34:48 -0400

    Create (557)reverseWords.js

---
+++ (557)reverseWords.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    let reversed = s.split(' ').map(function(word){
+        word = word.split('').reverse().join('');
+        return word;
+    });
+    return reversed.join(' ');
+};
