21254f7d5949bd35c494906074dd082724d13f1d
Chang Meng <mercyme@users.noreply.github.com>
2017-07-10 12:36:29 -0400

    Update (3)longest.js

--- (3)longest.js
+++ (3)longest.js
@@ -3,16 +3,18 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    var len = 0;
-    var maxLen = 0;
-    var str = '';
-    for(var i = 0;i < s.length;i++){
-        str += s.charAt(i);
-        len++;
-        if(str.indexOf(s.charAt(i)) != (len-1)){
-            str = str.substr(str.indexOf(s.charAt(i))+1, len-str.indexOf(s.charAt(i)));
-        }
-        maxLen = maxLen>len?maxLen:len;
+    var str = '',
+        len = 0,
+        maxLen = 0;
+    for(let i = 0;i < s.length;i++){
+      str += s.charAt(i);
+      len++;
+      if(str.indexOf(s.charAt(i)) != len - 1){
+        let firstOccurence = str.indexOf(s.charAt(i));
+        str = str.substr(firstOccurence + 1, len - firstOccurence);
+        len = str.length;
+      }
+      maxLen = maxLen > len ? maxLen : len;
     }
     return maxLen;
 };
