63d33245f3816590ca98831bffdcb25571eafac9
something new <mercyme@users.noreply.github.com>
2021-09-24 18:08:40 -0700

    Update (90)subsetWithDup.js

--- (90)subsetWithDup.js
+++ (90)subsetWithDup.js
@@ -2,33 +2,19 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
- var results, len, subSets;
-
-function subsetsHelper(nums, startIndex){
-    var lastRecursion = subSets.map((item) => item);
-    results.push(lastRecursion);
-
-    for(let i = startIndex; i < len; i++){
-        if(i !== 0 && nums[i] == nums[i-1] && i > startIndex){
-            continue;
-        }
-        subSets.push(nums[i]);
-        subsetsHelper(nums, i+1);
-        subSets.pop();
-    }
-}
 var subsetsWithDup = function(nums) {
-    results = [],
-    len = nums.length,
-    subSets = [];
-    
-    if(nums === null || nums.length === 0){
-        return results;
-    } 
-    
-    nums = nums.sort((a,b) => a-b);
-    
-    subsetsHelper(nums, 0);
-    
-    return results;
+  const results = [];
+  const helper = function (nums, index, subSet) {
+    results.push(subSet.concat());
+   
+    for(let i = index; i < nums.length; i++) {
+      subSet.push(nums[i]);
+      helper(nums, i+1,subSet);
+      subSet.pop():
+    }
+  }
+  
+  helper(nums, 0, []);
+ 
+  return results;
 };
